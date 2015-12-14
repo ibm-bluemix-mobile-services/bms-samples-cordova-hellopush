eb8b2e013279be27fdf55ac316e8c1f18372d92c
Matthew Parangot <mparang@us.ibm.com>
2015-12-14 14:02:44 -0600

    Front end clean

--- www/js/index.js
+++ www/js/index.js
@@ -19,8 +19,8 @@
 
 var app =  {
     // Bluemix credentials
-    route: "<APPLICATION ROUTE>",
-    guid: "<APPLICATION GUID>",
+    route: "http://HelloMatt.stage1.mybluemix.net",
+    guid: "9819f5be-fd58-467d-b214-b714c6b20df0",
 
     // Initialize BMSClient
     initialize: function() {
